579218e01c0353e6a52ce9d2052b6ceb80f8438a
Noah Giboney <noahgiboney@gmail.com>
2025-02-12 10:46:26 -0800

    added timer worker

--- CountdownTimer/app/src/main/java/com/example/countdowntimer/MainActivity.kt
+++ CountdownTimer/app/src/main/java/com/example/countdowntimer/MainActivity.kt
@@ -11,16 +11,42 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkRequest
+import androidx.work.workDataOf
 import com.example.countdowntimer.ui.theme.CountdownTimerTheme
 
 class MainActivity : ComponentActivity() {
+    private val timerViewModel = TimerViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CountdownTimerTheme {
-                TimerScreen()
+                TimerScreen(timerViewModel = timerViewModel)
             }
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        // Start TimerWorker if the timer is running
+        if (timerViewModel.isRunning) {
+            startWorker(timerViewModel.remainingMillis)
+        }
+    }
+
+    private fun startWorker(millisRemain: Long) {
+        val timerWorkRequest: WorkRequest = OneTimeWorkRequestBuilder<TimerWorker>()
+            .setInputData(
+                workDataOf(
+                    KEY_MILLIS_REMAINING to millisRemain
+                )
+            ).build()
+
+        WorkManager.getInstance(applicationContext).enqueue(timerWorkRequest)
     }
 }
